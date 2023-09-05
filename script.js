dccec46df9e8e001cd7f87d183209d74f0884371
Shawn Crook <shawncrook411@gmail.com>
2023-09-05 00:19:31 -0600

    testing

---
+++ script.js
@@ -0,0 +1,39 @@
+var pieces1 = [16];
+var pieces2 = [16];
+
+var board_size = 8;
+
+
+
+//Default Boar
+
+var player1[0] = {
+    piece: "pawn",
+    x-axis: "board[0][1]"
+}
+var player2 = 0; 
+
+
+
+
+
+
+
+
+console.log(pieces1, pieces2);
+
+//1 white / 2 black
+
+//[]2 piece type (p, n, b, r, q, k)
+//[]3 columnn (a, b, c, d, e, f, g ,h)
+//[]4 row (1, 2, 3, 4, 5, 6, 7, 8)
+setBoard(x)
+    {
+    // Sets pieces
+
+
+    // Sets pawns 
+     for(i = 0; i < board_size; i++)
+        var 
+
+    }
