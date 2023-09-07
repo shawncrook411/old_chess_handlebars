65ba8a3b377b4d0862d302f1200389ba5b523161
Shawn Crook <shawncrook411@gmail.com>
2023-09-07 01:40:52 -0600

    added funcitons setPieces and createBoard

--- script.js
+++ script.js
@@ -1,39 +1,95 @@
-var pieces1 = [16];
-var pieces2 = [16];
+var createColumn = document.createElement("div");
+var createSquare = document.createElement("div");
+var boardState = document.getElementById("board_border");
+var board_size;
+var columnArray = ["a", "b", "c", "d", "e", "f", "g", "h"];
 
-var board_size = 8;
-
-
-
-//Default Boar
-
-var player1[0] = {
-    piece: "pawn",
-    x-axis: "board[0][1]"
-}
-var player2 = 0; 
-
+//Piece type
+// 0 = rook
+// 1 = knight
+// 2 = bishop
+// 3 = queen
+// 4 = king
+// 5 = pawn
 
 
-
-
-
-
-
-console.log(pieces1, pieces2);
-
-//1 white / 2 black
-
-//[]2 piece type (p, n, b, r, q, k)
-//[]3 columnn (a, b, c, d, e, f, g ,h)
-//[]4 row (1, 2, 3, 4, 5, 6, 7, 8)
-setBoard(x)
+var createBoard = function (board_size){
+    for (let i = 0; i < board_size; i++)
     {
-    // Sets pieces
+            var x = document.createElement("div")
+            x.setAttribute("class", "column")
+                
+                for (let j = 0; j < board_size; j++)
+                {
+                    var y = document.createElement("div");
+                    y.setAttribute("class", "square");
 
-
-    // Sets pawns 
-     for(i = 0; i < board_size; i++)
-        var 
-
+                    if (i > 7)
+                    {
+                        y.setAttribute("id", (i+1) + ":" + (j+1));
+                        x.appendChild(y);
+                    }
+                    else
+                    {
+                        y.setAttribute("id", columnArray[i] + (j+1))
+                        x.appendChild(y);
+                    }
+                }
+            boardState.appendChild(x);
     }
+}
+var setPieces = function(color, type){
+    let y = [];    
+
+    if (type === 0)
+    {       
+        for(let i = 0; i < 5; i++)
+        {
+            var x = 
+            {
+                id: i * color,
+                piece_type: i,
+                color: color,
+                column: columnArray[i],
+                row: 4.5 - (color * 3.5)
+            }            
+            y.push(x);
+        }      
+
+        for (let i = 0; i < 3; i++)
+        {
+            var x =
+            {
+                id: (i+5) * color,
+                piece_type: 2 - i,
+                color: color,
+                column: columnArray[i+5],
+                row: 4.5 - (color * 3.5)
+            }
+            y.push(x);
+        }
+
+        for (let i = 0; i < 8; i++)
+        {
+            var x =
+            {
+                id: (i+8) * color,
+                piece_type: 5,
+                color: color,
+                column: columnArray[i],
+                row: 4.5 - (color * 2.5)
+            }
+            y.push(x);
+        }
+        return y;
+    }
+}
+
+createBoard(8);
+
+let player1 = setPieces (1, 0);
+let player2 = setPieces (-1, 0);
+
+console.log(player1);
+console.log(player2);
+
