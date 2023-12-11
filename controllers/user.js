aad3559925208466c0e6f28600200cb432165d60
Shawn Crook <shawncrook411@gmail.com>
2023-12-11 03:31:22 -0700

    create account route tested

--- controllers/user.js
+++ controllers/user.js
@@ -8,22 +8,24 @@ router.post('/createAccount', async (req, res) => {
         const newPassword = req.body.password
         const newEmail = req.body.email
 
+        if(!newEmail || !newPassword || !newUserName) {
+            res.status(400).json('Missing valid credentials') 
+            return
+        }
+
         const userData = await User.findOne({ where: sequelize.or( {username: newUserName}, {email: newEmail} )})
         if (userData){
             res.status(409).json('Username or email already exists')
             return
         }
 
+        const newUser = await User.create({
+            username: newUserName,
+            password: newPassword,
+            email: newEmail
+        })
         
-
-        // await User.create({
-        //     username: newUserName,
-        //     password: newPassword
-        // }).then(
-        //     (newUser) => {
-        //         res.status(200).json(`New User Created with ID: ${newUser.id} and Username: ${newUser.username} `)
-        //     }
-        // )
+        res.status(200).json(`New User Created with ID: ${newUser.id} and Username: ${newUser.username} `)       
 
     } catch(err) {
         console.log(err)
